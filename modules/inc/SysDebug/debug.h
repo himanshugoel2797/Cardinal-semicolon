bf176edbef098cf11b8ed3104ddfcc4b99e67c8b
Himanshu Goel <himanshu.goel2797@gmail.com>
2017-11-16 00:01:49 -0500

    Designing Debug console API.

---
+++ modules/inc/SysDebug/debug.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2017 Himanshu Goel
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#ifndef CARDINAL_SYSDEBUG_H
+#define CARDINAL_SYSDEBUG_H
+
+#include <stdint.h>
+
+typedef int (*DebugFunc) ();
+
+typedef struct {
+    char func_name[240];
+    DebugFunc func;
+} DebugHandler_t;
+
+typedef enum {
+    DebugColor_Black,
+    DebugColor_White,
+    DebugColor_Red,
+    DebugColor_Green,
+    DebugColor_Blue,
+} DebugColor_t;
+
+int register_debughandler(DebugHandler_t *handler);
+int unregister_debughandler(DebugHandler_t *handler);
+
+int debug_printstr(const char *str);
+
+int debug_readstr(char *buf, int maxlen);
+
+int debug_setfg(DebugColor_t col);
+int debug_setbg(DebugColor_t col);
+
+#endif
