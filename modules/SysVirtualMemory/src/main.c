fa2d3cd2a245df9a8d2856a177375f54d91ff21a
Himanshu Goel <himanshu.goel2797@gmail.com>
2017-11-16 00:58:18 -0500

    Started designing virtual memory, update notes.

---
+++ modules/SysVirtualMemory/src/main.c
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2017 Himanshu Goel
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+int module_init(){
+    //virtual memory code is platform specific recursive implementation for 64-bit
+    //vmem_map
+    //vmem_unmap
+    //vmem_create
+    //vmem_setactive
+    //vmem_virttophys
+    //vmem_phystovirt
+    //vmem_init
+
+    //add task priority options to each method
+    //TODO: update physical memory code to include task priority flags
+
+    //define standard functions which are used to setup the kernel's desired mapping of memory
+    return 0;
+}
