1e1e849287fd080138adcf16935490a87e2d8741
Himanshu Goel <himanshu.goel2797@gmail.com>
2018-06-28 05:05:37 -0400

    Start planning CoreStorage.

---
+++ servers/CoreStorage/src/main.c
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2018 hgoel
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+//TODO: register/deregister storage devices
+//TODO: register file system providers
+//TODO: handle file IO syscalls
+
+int module_init(){
+
+    return 0;
+}
