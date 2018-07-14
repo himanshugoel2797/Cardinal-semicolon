d170d5b85b844a056718fd38fe07173350f1fe73
Himanshu Goel <himanshu.goel2797@gmail.com>
2018-07-14 01:27:35 -0400

    Work on driver design.

---
+++ drivers/intel_gfx/inc/devices.h
@@ -0,0 +1,22 @@
+// Copyright (c) 2018 Himanshu Goel
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#ifndef CARDINALSEMI_INTEL_GFX_DEVICES_H
+#define CARDINALSEMI_INTEL_GFX_DEVICES_H
+
+#include <stdint.h>
+
+typedef struct {
+    char *name;
+    uint16_t device_id;
+    uintptr_t bar_virt;
+} intel_gfx_device_t;
+
+intel_gfx_device_t hsw = {
+    .name = "Intel Haswell-ULT Integrated Graphics",
+    .device_id = 0x0a16,
+};
+
+#endif
