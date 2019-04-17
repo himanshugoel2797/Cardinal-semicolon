7ce931035757c5923f094cf0f04b457351abdb1e
Himanshu Goel <himanshu.goel2797@gmail.com>
2019-04-17 07:35:05 -0400

    Update build system to work under Ubuntu.

--- platform/x86_64/pc/flags.cmake
+++ platform/x86_64/pc/flags.cmake
@@ -27,13 +27,13 @@ COMMAND tar -cvf "ISO/isodir/boot/initrd" -C "ISO/isodir/boot" .
 COMMAND cp "kernel/kernel.bin" "ISO/isodir/boot/kernel.bin"
 COMMAND mkdir -p "ISO/isodir/boot/grub"
 COMMAND cp "${CMAKE_CURRENT_SOURCE_DIR}/platform/x86_64/pc/grub.cfg" "ISO/isodir/boot/grub/grub.cfg"
-COMMAND grub2-mkrescue -d /usr/lib/grub/i386-pc -o "ISO/os.iso" "ISO/isodir"
+COMMAND grub-mkrescue -d /usr/lib/grub/i386-pc -o "ISO/os.iso" "ISO/isodir"
 DEPENDS kernel.bin)
 
 add_custom_target(disk.img
     COMMAND qemu-img create -f raw disk.img 128M)
 
 add_custom_target(run 
-    COMMAND qemu-system-x86_64 --enable-kvm -m 1G -machine q35, -cpu host -smp 3 -d int,cpu_reset,guest_errors -drive id=disk,file=disk.img,if=none -device ahci,id=ahci -device ide-drive,drive=disk,bus=ahci.0 -net nic,model=virtio -net bridge,br=virbr0 -device ich9-intel-hda -device hda-output -device ich9-usb-uhci3 -cdrom "ISO/os.iso" -boot d -device virtio-vga,virgl=on -display gtk,gl=on
+    COMMAND qemu-system-x86_64 --enable-kvm -m 1G -machine q35, -cpu host -smp 3 -d int,cpu_reset,guest_errors -drive id=disk,file=disk.img,if=none -device ahci,id=ahci -device ide-drive,drive=disk,bus=ahci.0 -net nic,model=virtio -device ich9-intel-hda -device hda-output -device ich9-usb-uhci3 -cdrom "ISO/os.iso" -boot d -device virtio-vga,virgl=on
     DEPENDS image
     DEPENDS disk.img)
