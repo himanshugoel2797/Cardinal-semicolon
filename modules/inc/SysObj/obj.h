a0f19483eb8a6af1b754442c1dc2fa5bfd2bff73
Himanshu Goel <himanshu.goel2797@gmail.com>
2019-04-24 20:25:46 -0400

    Started planning on SysObj

---
+++ modules/inc/SysObj/obj.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2017 Himanshu Goel
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#ifndef CARDINAL_SYSOBJ_H
+#define CARDINAL_SYSOBJ_H
+
+#include <types.h>
+
+#endif
