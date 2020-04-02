bcc9bc339dc0edaab60346eef766a03f7312f279
Himanshu Goel <himanshu.goel2797@gmail.com>
2020-04-02 13:06:18 -0400

    Work on UHCI driver.

--- drivers/uhci/inc/uhci.h
+++ drivers/uhci/inc/uhci.h
@@ -8,9 +8,19 @@
 
 #include <stdint.h>
 
+#define USBCMD_REG (0x00)
+#define USBSTS_REG (0x02)
+#define USBINTR_REG (0x04)
+#define FRNUM_REG (0x06)
+#define FRBASEADDR_REG (0x08)
+#define SOFMOD_REG (0x0C)
+#define PORTSCn_REG(x) (0x10 + (x * 2))
+
 typedef struct uhci_ctrl_state
 {
     uint32_t iobar;
+    uint32_t framelist_pmem;
+    uint32_t *framelist;
 
     struct uhci_ctrl_state *next;
 } uhci_ctrl_state_t;
--- drivers/uhci/src/main.c
+++ drivers/uhci/src/main.c
@@ -13,6 +13,7 @@
 #include "SysVirtualMemory/vmem.h"
 #include "SysPhysicalMemory/phys_mem.h"
 #include "SysInterrupts/interrupts.h"
+#include "SysTimer/timer.h"
 #include "pci/pci.h"
 
 #include "uhci.h"
@@ -20,9 +21,55 @@
 static uhci_ctrl_state_t *instances = NULL;
 static int instance_lock = 0;
 
+static void write8(uhci_ctrl_state_t *state, uint16_t addr, uint8_t val)
+{
+    outb(state->iobar + addr, val);
+}
+
+static void write16(uhci_ctrl_state_t *state, uint16_t addr, uint16_t val)
+{
+    outw(state->iobar + addr, val);
+}
+
+static void write32(uhci_ctrl_state_t *state, uint16_t addr, uint32_t val)
+{
+    outl(state->iobar + addr, val);
+}
+
+static uint8_t read8(uhci_ctrl_state_t *state, uint16_t addr)
+{
+    return inb(state->iobar + addr);
+}
+
+static uint16_t read16(uhci_ctrl_state_t *state, uint16_t addr)
+{
+    return inw(state->iobar + addr);
+}
+
+static uint32_t read32(uhci_ctrl_state_t *state, uint16_t addr)
+{
+    return inl(state->iobar + addr);
+}
+
+static void uhci_reset(uhci_ctrl_state_t *state)
+{
+    write16(state, USBCMD_REG, 1 << 2);
+    timer_wait(10 * 1000 * 1000);
+    write32(instance, FRBASEADDR_REG, instance->framelist_pmem);
+    write16(state, 1);
+}
+
+static void uhci_enableport(uhci_ctrl_state_t *state, int idx)
+{
+    uint16_t v = read16(state, PORTSCn_REG(idx));
+    v = (v & ~(1 << 9));  //Bring port out of reset
+    v = (v & ~(1 << 12)); //Bring port out of suspend
+    v = (v | (1 << 2));   //Enable port
+    write16(state, PORTSCn_REG(idx), v);
+}
+
 int module_init(void *ecam_addr)
 {
-
     pci_config_t *device = (pci_config_t *)vmem_phystovirt((intptr_t)ecam_addr, KiB(4), vmem_flags_uncached | vmem_flags_kernel | vmem_flags_rw);
 
     //enable pci bus master
@@ -39,6 +86,22 @@ int module_init(void *ecam_addr)
     sti(cli_state);
 
     uint64_t bar = (device->bar[4] & 0xFFFFFFF0); //I/O space BAR
+    instance->iobar = bar;
+
+    //Frame list: 4x1024 entries
+    //Each frame contains transfer descriptors
+    //Queue Heads are for bulk transfers
+    instance->framelist_pmem = (uint32_t)pagealloc_alloc(0, 0, physmem_alloc_flags_32bit | physmem_alloc_flags_data | physmem_alloc_flags_zero, KiB(4));
+    instance->framelist = (uint32_t *)vmem_phystovirt((intptr_t)instance->framelist_pmem, KiB(4), vmem_flags_uncached | vmem_flags_kernel | vmem_flags_rw);
+
+    //Reset HCI
+    uhci_reset(instance);
+
+    //Start polling process
+
+    //Enable all ports
+    uhci_enableport(instance, 0);
+    uhci_enableport(instance, 1);
 
     return 0;
 }
